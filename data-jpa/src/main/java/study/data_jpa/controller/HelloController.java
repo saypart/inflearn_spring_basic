7798a124bfdf654f540ea0552faba0a8b00ad550
sanghyeon <saypart@gmail.com>
2024-09-07 21:24:03 +0900

    김영한 스프링 데이터 JPA 기본 세팅

---
+++ data-jpa/src/main/java/study/data_jpa/controller/HelloController.java
@@ -0,0 +1,15 @@
+package study.data_jpa.controller;
+
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+}
